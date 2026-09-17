78ca17a63f3a3c3f1d7aa746eef028f0743a1081
TonBunHeng <tonbunheng1122@gmail.com>
2026-09-18 00:02:14 +0700

    feat(cors): add port 5175 to allowed origins for tourism-travel

--- config/cors.php
+++ config/cors.php
@@ -26,6 +26,8 @@ return [
         'http://127.0.0.1:3000',
         'http://localhost:5174',
         'http://127.0.0.1:5174',
+        'http://localhost:5175',
+        'http://127.0.0.1:5175',
     ],
 
     'allowed_origins_patterns' => [
